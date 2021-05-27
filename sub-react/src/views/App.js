b71581624ab273df9c789c9de46bdcdafc98e7b0
BoBoooooo <17746714@qq.com>
2021-05-27 14:27:12 +0800

    feat: sub-react新增antd

--- sub-react/src/views/App.js
+++ sub-react/src/views/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "../assets/logo.svg";
 import styles from "../styles/app.module.css";
 import { Link } from "dva/router";
-
+import Antd from "./Antd";
 function App() {
   return (
     <div className="App">
       <img src={logo} className={styles.logo} alt="logo" />
       <div>
-        <Link to={"/hello"}>Hello</Link> | 
-        <Link to={"/test"}>Test</Link>
+        <Link to={"/hello"}>Hello</Link> |<Link to={"/test"}>Test</Link>
+        <Antd></Antd>
       </div>
     </div>
   );
